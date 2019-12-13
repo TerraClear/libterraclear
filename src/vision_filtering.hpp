77dfa06341442edfe9ee469d1e5c9e5892eae4dc
Anisha Uppugonduri <anishau@uw.edu>
2019-12-12 18:39:01 -0800

    updates to tf matrix saving

--- src/vision_filtering.hpp
+++ src/vision_filtering.hpp
@@ -18,7 +18,6 @@
  * CREATED BY: Koos du Preez - koos@terraclear.com
  * 
 */
-
 #include <iostream>
 #include <math.h>
 #include <algorithm>
@@ -52,7 +51,9 @@ namespace terraclear
             static cv::cuda::GpuMat apply_normalization_rgb_gpu(cv::cuda::GpuMat& src_img);
             static cv::cuda::GpuMat apply_gpu_threshold(cv::cuda::GpuMat& src_img, cv::Scalar lowrange, cv::Scalar highrange, bool use_hsv = true);
             static void             apply_gpu_color_contouring(cv::Mat& src_img,  cv::Mat& dst_img, cv::Scalar lowrange, cv::Scalar highrange, bool use_hsv = true);
-            static cv::Mat          apply_gpu_warp(cv::Mat src_img, cv::Size dst_size, std::vector<cv::Point> pts_src, std::vector<cv::Point> pts_dst);
+            static cv::Mat          apply_gpu_warp(cv::Mat src_img, cv::Size dst_size, cv::Mat h_mat);
+          static cv::Mat          apply_gpu_warp(cv::Mat src_img, cv::Size dst_size, std::vector<cv::Point> pts_src, std::vector<cv::Point> pts_dst);
+
             static cv::Mat          apply_gpu_rotate(cv::Mat src_img, float rotation_angle);
 
             //cpu fitlers and transforms
